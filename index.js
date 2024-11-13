f91351ff73cb498e342b48a060cb7d76e5e0b655
Fiorella Marky <fiorella.marky@gmail.com>
2024-11-13 16:30:38 -0500

    add API functionality (POST comment, DELETE comment), fix timestamp, add validation middleware

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 import express from "express";
 import videosRoute from "./routes/videos.js";
 import "dotenv/config";
+import cors from "cors";
 
 const { BACKEND_URL, PORT, CORS_ORIGIN } = process.env;
 const app = express();
 
 // middleware
-app.use(cors()); //{ origin: CORS_ORIGIN }
+app.use(cors()); //
 app.use(express.json());
 app.use(express.static("public")); // serve images/ as public static asset
 
--- routes/videos.js
+++ routes/videos.js
@@ -6,6 +6,17 @@ const router = express.Router();
 
 const FILE_PATH = "./data/videos.json";
 const videoData = JSON.parse(fs.readFileSync(FILE_PATH));
+const updateData = () => fs.writeFileSync(FILE_PATH, JSON.stringify(videoData));
+
+const validate = (req, res, next) => {
+  const { id } = req.params;
+  req.findVid = videoData.find((video) => video.id === id);
+  if (req.findVid) {
+    next();
+  } else {
+    res.status(400).send(`Invalid video ID: ${id}`);
+  }
+};
 
 router
   .route("/")
@@ -24,38 +35,60 @@ router
   })
   .post((req, res) => {
     try {
-      const { title, description } = req.body;
+      const { title, description, image } = req.body;
       const newVid = {
         id: uuid(),
         title,
         channel: "Fio M",
-        image: "https://placecats.com/800/500",
+        image,
         description,
         views: "0",
         likes: "0",
         duration: "4:33",
         video: "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
-        timestamp: new Date(),
+        timestamp: Date.now(),
         comments: [],
       };
       videoData.push(newVid);
-      fs.writeFileSync(FILE_PATH, JSON.stringify(videoData));
+      updateData();
       res.status(200).send(`Video added: ${newVid}`);
     } catch (e) {
       res.status(400).send(`Unable to add video: ${newVid}`);
     }
   });
 
-router.route("/:id").get((req, res) => {
-  const { id } = req.params;
-  try {
-    const findVid = videoData.find((video) => video.id === id);
-    res.status(200).send(findVid);
-  } catch (e) {
-    res.send(400).send(`No video found with id ${id}`);
-  }
-  res.send("video details");
+router.get("/:id", validate, (req, res) => {
+  res.status(200).send(req.findVid);
 });
 
-// router.route("/:id/comments").post((req, res) => {});
+router.post("/:id/comments", validate, (req, res) => {
+  const { name, comment } = req.body;
+
+  if (!name || !comment) {
+    res.status(400).send(`Comment information is incomplete: ${{ name, comment }}`);
+  }
+
+  const newComment = {
+    id: uuid(),
+    name,
+    comment,
+    timestamp: Date.now(),
+  };
+
+  req.findVid.comments.push(newComment);
+  updateData();
+  const findComment = req.findVid.comments.find(
+    (comment) => comment.id === newComment.id
+  );
+  res.status(200).send(`Comment posted: ${findComment}}`);
+});
+
+router.delete("/:id/comments/:commentId", validate, (req, res) => {
+  const { commentId } = req.params;
+  const commentArray = req.findVid.comments;
+  const commentIndex = commentArray.findIndex((comment) => comment.id === commentId);
+  commentArray.splice(commentIndex, 1);
+  updateData();
+});
+
 export default router;
